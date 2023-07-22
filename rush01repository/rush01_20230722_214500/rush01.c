63084bf0a7709d688f9107d014710194306e669d
papad21297 <54827041+papad21297@users.noreply.github.com>
2023-07-23 01:15:55 +0700

    2023.07.23 01:15:50

--- rush01repository/rush01_20230722_214500/rush01.c
+++ rush01repository/rush01_20230722_214500/rush01.c
@@ -87,7 +87,7 @@ void	itrate_lft_to_rght_lne(int n, char *argv)
 		filter_candidates(i, &candidate_amount);
 		// arr_arr = (int *)malloc(candidate_amount * sizeof(int));
 		// ptr_arr_arr = &arr_arr;
-		// arr_arr = collect_candidates(candidates_collected, i, arr_arr);
+		arr_arr = collect_candidates(candidates_collected, i, arr_arr);
 		candidates_collected += candidate_amount;
 		arr_arr_size[i] = candidate_amount;
 		printf("#%d %d => ???? <= %d\n", i, argv[(4 * n) + (2 * i)] - 48,
