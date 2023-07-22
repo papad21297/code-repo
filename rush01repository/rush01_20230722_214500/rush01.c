ac45cf9c0637cc76d9fc2f909cebf505d665521a
papad21297 <54827041+papad21297@users.noreply.github.com>
2023-07-23 00:44:11 +0700

    2023.07.23 00:44:08

--- rush01repository/rush01_20230722_214500/rush01.c
+++ rush01repository/rush01_20230722_214500/rush01.c
@@ -132,7 +132,7 @@ void	collect_candidates(int candidate_amount, int i, int *ptr_arr_arr)
 	{
 		if (candidacy_array[_i] % candidacy_volume[i] == 0)
 		{
-			(*ptr_arr_arr)[candidate_amount + matching_found] = candidacy_array[_i];
+			ptr_arr_arr[candidate_amount + matching_found] = candidacy_array[_i];
 			printf("candidacy_array[%d]: \"%d\"\n", candidate_amount + matching_found,
 				candidacy_array[_i]);
 			matching_found++;
