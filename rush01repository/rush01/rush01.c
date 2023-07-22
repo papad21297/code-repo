91b253a03277727c3da3b868b8e4cfd33f7f4cad
papad21297 <54827041+papad21297@users.noreply.github.com>
2023-07-22 16:11:46 +0700

    2023.07.22 16:11:39

--- rush01repository/rush01/rush01.c
+++ rush01repository/rush01/rush01.c
@@ -80,13 +80,9 @@ void	permutation_iteration(int n, int formation_number, int lft, int rght)
 	while (i < formation_number)
 	{
 		index_to_placement_conversion(val_arr, n, i);
-		if (
-			(lft == prmttion_mmber_itration(val_arr, n))
-			&& (rght == bckwrd_prmttion_mmber_itration(val_arr, n))
-		)
-			printf("[%d] %d => ... <= %d", i,
-				prmttion_mmber_itration(val_arr, n),
-				bckwrd_prmttion_mmber_itration(val_arr, n));
+		printf("[%d] %d => ... <= %d", i,
+			(lft == prmttion_mmber_itration(val_arr, n)),
+			(rght == bckwrd_prmttion_mmber_itration(val_arr, n));
 		printf("\n");
 		i++;
 	}
