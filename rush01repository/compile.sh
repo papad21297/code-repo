d7b39399fb257a728cf4dd056d65e4d921aec657
papad21297 <54827041+papad21297@users.noreply.github.com>
2023-07-22 15:38:52 +0700

    2023.07.22 15:38:48

--- rush01repository/compile.sh
+++ rush01repository/compile.sh
@@ -3,5 +3,5 @@ clear;
 git pull;
 # ~/.local/bin/norminette -R CheckForbiddenSourceHeader;
 # cc -Wall -Wextra -Werror rush01/*.c -o main.o;
-cc rush01/*.c -o main.o "4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2";
-./main.o | cat -e
+cc rush01/*.c -o main.o;
+./main.o "4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2" | cat -e
