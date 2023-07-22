7c47af99d464f9635081c1eda9ff483076eee002
papad21297 <54827041+papad21297@users.noreply.github.com>
2023-07-22 22:06:32 +0700

    2023.07.22 22:06:28

--- rush01repository/rush01_20230722_214500/rush01.c
+++ rush01repository/rush01_20230722_214500/rush01.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 {
     if ((argc - 1) == 1)
     {
-        printf("your first and only argument is \"%s\"", argv[1 + 1]);
+        printf("your first and only argument is \"%s\"", argv[0 + 1]);
     }
 	else
 		printf("the number of your argument(s) isn't exactly 1");
