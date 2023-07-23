f9950abe3ef01a217db0aba5ca883d1180994f6f
papad21297 <54827041+papad21297@users.noreply.github.com>
2023-07-23 10:41:33 +0700

    2023.07.23 10:41:22

--- rush01repository/compile.sh
+++ rush01repository/compile.sh
@@ -5,10 +5,10 @@
 # python_works\copy_current_date_and_time_
 # to_clipboard\copy_current_date_and_time_
 # to_clipboard.py
-clear;
 git pull;
 # ~/.local/bin/norminette -R CheckForbiddenSourceHeader;
 # git pull&&clear&&norminette -R CheckForbiddenSourceHeader rush01;
 # cc -Wall -Wextra -Werror rush01/*.c -o main.o;
+clear;
 cc rush01repository/rush01_20230723_103000/*.c -o rush01repository/main.o;
 rush01repository/main.o "1 2 2 3 3 1 3 2 1 2 4 2 4 2 1 2" | cat -e
