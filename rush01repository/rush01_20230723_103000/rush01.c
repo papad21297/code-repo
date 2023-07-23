96482f188bd255fc8f4b658a0d1fa37184cc22df
papad21297 <54827041+papad21297@users.noreply.github.com>
2023-07-23 10:52:14 +0700

    2023.07.23 10:52:11

--- rush01repository/rush01_20230723_103000/rush01.c
+++ rush01repository/rush01_20230723_103000/rush01.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
-int main(void)
+void solve_puzzle(void);
+
+int main(int argc, char *argv[])
 {
-    printf("01234");
+    if (argc == 2)
+        solve_puzzle();
+    else
+        write(1, "invalid amount of input(s)", 26);
     return (0);
+}
+
+void solve_puzzle(void)
+{
+    // code...
+    int n;
+    int i;
+
+    n = 4
+    i = 0;
+    while (i < n)
+    {
+        printf("%d\n", %d);
+        i++;
+    }
 }
