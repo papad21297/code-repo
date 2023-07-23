37d9bfdd7668e410fe13624052a4d3120d4ce1e0
papad21297 <54827041+papad21297@users.noreply.github.com>
2023-07-23 11:58:19 +0700

    2023.07.23 11:58:15

--- rush01repository/rush01_20230723_103000/rush01.c
+++ rush01repository/rush01_20230723_103000/rush01.c
@@ -4,9 +4,9 @@
 int main(int argc, char *argv[])
 {
    printf("%d", argc);
-   printf("item #1: %s", argv[0]);
-   printf("item #2: %s", argv[1]);
-   printf("item #3: %s", argv[2]);
+   printf("item #1: %s\n", argv[0]);
+   printf("item #2: %s\n", argv[1]);
+   printf("item #3: %s\n", argv[2]);
    printf("item #4: %s", argv[3]);
    return (0);
 }
