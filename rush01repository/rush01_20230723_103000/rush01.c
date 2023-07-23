ad5e77ce8adcb38a9f5e094b64fb2e5866d73fcd
papad21297 <54827041+papad21297@users.noreply.github.com>
2023-07-23 11:54:47 +0700

    2023.07.23 11:54:40

--- rush01repository/rush01_20230723_103000/rush01.c
+++ rush01repository/rush01_20230723_103000/rush01.c
@@ -1,45 +1,8 @@
 #include <stdio.h>
-#include <unistd.h>
 
-void solve_puzzle(char *input);
-void render_each_horizintal_row(int n, char *input);
 
 int main(int argc, char *argv[])
 {
-    if (argc == 2)
-    {
-        printf("received input: \"%s\"\n", argv[1]);
-        solve_puzzle(argv[1]);
-    }
-    else
-        write(1, "invalid amount of input(s)", 26);
-    return (0);
-}
-
-void solve_puzzle(char *input)
-{
-    // code...
-    int n;
-    int i;
-
-    n = 4;
-    i = 0;
-    while (i < n)
-    {
-        render_each_horizintal_row(n, input);
-        i++;
-    }
-}
-
-void render_each_horizintal_row(int n, char *input)
-{
-    int i;
-
-    i = 0;
-    while (i < n)
-    {
-        printf("%d => X X X X <= %d\n", input[(4 * n) + (2 * i)] - 48,
-            input[(6 * n) + (2 * i)] - 48);
-        i++;
-    }
+   printf("01234");
+   return (0);
 }
