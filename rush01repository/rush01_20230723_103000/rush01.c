ebd1bcc03415532dba8ba3f82b6c591b8c1937d6
papad21297 <54827041+papad21297@users.noreply.github.com>
2023-07-23 12:01:22 +0700

    2023.07.23 12:01:18

--- rush01repository/rush01_20230723_103000/rush01.c
+++ rush01repository/rush01_20230723_103000/rush01.c
@@ -3,10 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-   printf("%d", argc);
-   printf("item #1: %s\n", argv[0]);
-   printf("item #2: %s\n", argv[1]);
-   printf("item #3: %s\n", argv[2]);
-   printf("item #4: %s", argv[3]);
-   return (0);
+    if (argc == 2)
+    {
+        printf("%s", argv[1]);
+    }
+    else
+    {
+        printf("error...");
+    }
+    return (0);
 }
