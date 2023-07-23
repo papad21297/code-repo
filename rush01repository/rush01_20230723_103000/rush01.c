74cc8e58a03fad4fb753728c5e3a3b7c47eedb89
papad21297 <54827041+papad21297@users.noreply.github.com>
2023-07-23 12:20:51 +0700

    2023.07.23 12:20:44

--- rush01repository/rush01_20230723_103000/rush01.c
+++ rush01repository/rush01_20230723_103000/rush01.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-void solve_puzzle(char *input);
+int     count_input_length(char *input);
+void    solve_puzzle(char *input);
 
 int main(int argc, char *argv[])
 {
-    if (argc == 2)
+    int n;
+
+    n = 4;
+    if ((argc == 2) && (count_input_length(argv[1]) == (2 * (n * 4)) - 1))
     {
         solve_puzzle(argv[1]);
     }
@@ -18,4 +22,18 @@ int main(int argc, char *argv[])
 void solve_puzzle(char *input)
 {
     printf("your input: \"%s\"", input);
+}
+
+int    count_input_length(char *input)
+{
+    int length;
+    int i;
+
+    i = 0;
+    while (input[i] != '\0')
+    {
+        length++;
+        i++;
+    }
+    return length;
 }
