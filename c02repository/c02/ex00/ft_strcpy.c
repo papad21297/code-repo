a576b261fcdb85657ba5a081d09897cdccb15ff2
Papad Ngaothammasakun <papangao@z2t8c1.42bangkok.com>
2023-07-28 15:07:26 +0700

    .

---
+++ c02repository/c02/ex00/ft_strcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: papangao <papangao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/28 14:49:39 by papangao          #+#    #+#             */
+/*   Updated: 2023/07/28 14:55:43 by papangao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	char	*original_dest;
+	
+	original_dest = dest;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (original_dest);
+}
---
+++ c02repository/subject.md
@@ -0,0 +1,31 @@
+## Files to turn in
+`ft_strcpy.c`
+
+---
+
+## Allowed functions
+`None`
+
+---
+
+- Reproduce the behavior of the function `strcpy` (man strcpy)
+- for loop is forbidden, so use while loop instead
+- variable declaration and its value assigning should be done on separate line (like
+
+```c
+int i = 0;
+```
+
+  should be modified to be like
+
+```c
+int i;
+
+i = 0;
+```
+  )
+- Here's how it should be prototyped:
+
+```c
+	char	*ft_strcpy(char *dest, char *src);
+```
